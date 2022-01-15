107ec29c1093903f3ef4e61ac6abdcbbaf38e56e
root <root@ip-172-31-45-153.ap-south-1.compute.internal>
2022-01-15 09:25:10 +0000

    sample commit 1 by Rakesh

---
+++ Sample.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
+import javax.swing.UIManager;
+
+
+public class WordCount {
+
+    public static void main(String[] args) {
+        countWordsViaGUI();
+    }
+
+
+    // allow user to pick file to exam via GUI.
+    // allow multiple picks
+    public static void countWordsViaGUI() {
+        setLookAndFeel();
+        try {
+            Scanner key = new Scanner(System.in);
+            do {
+                System.out.println("Opening GUI to choose file.");
+                Scanner fileScanner = new Scanner(getFile());
+                Stopwatch st = new Stopwatch();
+                st.start();
+                ArrayList<String> words = countWordsWithArrayList(fileScanner);
+                st.stop();
+                System.out.println("time to count: " + st);
+                System.out.print("Enter number of words to display: ");
+                int numWordsToShow = Integer.parseInt(key.nextLine());
+                showWords(words, numWordsToShow);
+                fileScanner.close();
+                System.out.print("Perform another count? ");
+            } while(key.nextLine().toLowerCase().charAt(0) == 'y');
+            key.close();
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Problem reading the data file. Exiting the program." + e);
+        }
+    }
